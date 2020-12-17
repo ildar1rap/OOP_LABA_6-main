ce0b1bb19b954bb5b696780242819bf5d8c834df
ildar1rap <61325972+ildar1rap@users.noreply.github.com>
2020-12-17 15:57:45 +0500

    Добавьте файлы проекта.

---
+++ OOP_LABA_6/Form1.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace OOP_LABA_6
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.paint_box = new System.Windows.Forms.Panel();
+            this.label_paintbox = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.paint_box.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // paint_box
+            // 
+            this.paint_box.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.paint_box.BackColor = System.Drawing.SystemColors.Info;
+            this.paint_box.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.paint_box.Controls.Add(this.label_paintbox);
+            this.paint_box.Location = new System.Drawing.Point(9, 10);
+            this.paint_box.Margin = new System.Windows.Forms.Padding(2);
+            this.paint_box.Name = "paint_box";
+            this.paint_box.Size = new System.Drawing.Size(628, 533);
+            this.paint_box.TabIndex = 0;
+            this.paint_box.MouseClick += new System.Windows.Forms.MouseEventHandler(this.paint_box_MouseClick);
+            // 
+            // label_paintbox
+            // 
+            this.label_paintbox.AutoSize = true;
+            this.label_paintbox.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_paintbox.Location = new System.Drawing.Point(3, 3);
+            this.label_paintbox.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label_paintbox.Name = "label_paintbox";
+            this.label_paintbox.Size = new System.Drawing.Size(157, 21);
+            this.label_paintbox.TabIndex = 0;
+            this.label_paintbox.Text = "Окно для рисования";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.radioButton1.Location = new System.Drawing.Point(3, 13);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(60, 24);
+            this.radioButton1.TabIndex = 7;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Круг";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.radioButton2.Location = new System.Drawing.Point(3, 43);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(75, 24);
+            this.radioButton2.TabIndex = 8;
+            this.radioButton2.Text = "Линия";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.radioButton3.Location = new System.Drawing.Point(3, 73);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(119, 24);
+            this.radioButton3.TabIndex = 9;
+            this.radioButton3.Text = "radioButton3";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.Controls.Add(this.radioButton1);
+            this.panel1.Controls.Add(this.radioButton3);
+            this.panel1.Controls.Add(this.radioButton2);
+            this.panel1.Location = new System.Drawing.Point(642, 14);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(161, 113);
+            this.panel1.TabIndex = 10;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.BackColor = System.Drawing.Color.LawnGreen;
+            this.textBox1.Location = new System.Drawing.Point(642, 251);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 12;
+            this.textBox1.Click += new System.EventHandler(this.textBox4_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox2.BackColor = System.Drawing.Color.Aqua;
+            this.textBox2.Location = new System.Drawing.Point(642, 277);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 13;
+            this.textBox2.Click += new System.EventHandler(this.textBox4_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox3.BackColor = System.Drawing.Color.Fuchsia;
+            this.textBox3.Location = new System.Drawing.Point(642, 303);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 14;
+            this.textBox3.Click += new System.EventHandler(this.textBox4_Click);
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(642, 133);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(161, 40);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Изменение цвета";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox4.Location = new System.Drawing.Point(645, 179);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 15;
+            this.textBox4.Click += new System.EventHandler(this.textBox4_Click);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(844, 552);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.paint_box);
+            this.KeyPreview = true;
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Main";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Main_KeyDown);
+            this.paint_box.ResumeLayout(false);
+            this.paint_box.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+        private System.Windows.Forms.Panel paint_box;
+        private System.Windows.Forms.Label label_paintbox;
+        #endregion
+
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.TextBox textBox4;
+    }
+}
+
---
+++ OOP_LABA_6/Form1.cs
@@ -0,0 +1,394 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OOP_LABA_6
+{
+    public partial class Main : Form
+	{
+		mystorage Storage = new mystorage();
+		Pen pen = new Pen(Color.Red, 3);
+		public Main()
+        {
+			InitializeComponent();
+        }
+		public class FigureIF //класс фигур
+		{
+			private int x, y; // точки
+			private Color FigureIFColor = Color.Red;//цвет;
+			private FigureIF _next;//указатель на некст объект
+			public FigureIF(int x, int y)//конструктор с параметрами
+			{
+				this.x = x;
+				this.y = y;
+			}
+			public int getX()
+			{
+				return x;
+			}
+			public void setX(int k)
+			{
+				x+=k;
+			}
+			public int getY()
+			{
+				return y;
+			}
+			public void setY(int k)
+			{
+				y += k;
+			}
+			public Color getColor()
+			{
+				return FigureIFColor;
+			}
+			public void setColor(Color changeColor)
+			{
+				FigureIFColor = changeColor;
+			}
+			public void setnext(FigureIF obj)//метод set next
+			{//задаем _next
+				_next = obj;
+			}
+			public FigureIF getnext() //метод возвращающий указатель не след.
+			{//возвращает _next
+				return _next;
+			}
+			public virtual void FactoryPaint(Panel paint_box, FigureIF figure, Pen pen) 
+			{ } //фабричный метож
+			public virtual void moveX(int k)
+			{
+				x += k;
+			}
+			public virtual void moveY(int k)
+			{
+				y += k;
+			}
+			public virtual void changeSize(int k) { }
+		}
+		public void printFigure(FigureIF obj, Color color)//метод для отрисовки 
+		{
+			obj.setColor(color);
+			obj.FactoryPaint(paint_box, obj, pen);
+		}
+		class Line : FigureIF
+		{
+			private int LineLength = 50; // длина линии
+			public Line(int x, int y) : base(x, y)//конструктор с параметрами
+			{
+
+			}
+			public int getLineLength()
+			{
+				return LineLength;
+			}
+			public override void FactoryPaint(Panel paint_box, FigureIF figure, Pen pen)
+			{
+				pen.Color = figure.getColor();//перекрываю метод, фабричного метода
+				paint_box.CreateGraphics().DrawLine(pen, figure.getX() - getLineLength(),
+					figure.getY(), figure.getX() + getLineLength(), figure.getY());
+			}
+			public override void changeSize(int k) 
+			{
+				LineLength += k;
+			}
+			public override void moveX(int k)
+			{
+				if (getX() - LineLength > 0 )
+				{
+					this.setX(k);
+				}
+				else
+					this.setX(Math.Abs(k));
+			}
+			public override void moveY(int k)
+			{
+				if (getY()> 0)
+				{
+					this.setY(k);
+				}
+				else
+					this.setY(Math.Abs(k));
+			}
+		}
+
+		class Сircle : FigureIF
+        {
+			private int СircleRad = 30; //радиус круга
+            public Сircle(int x, int y) : base(x,y)//конструктор с параметрами
+            {
+
+            }
+			public int getRad()
+			{
+				return СircleRad;
+			}
+			public override void FactoryPaint(Panel paint_box, FigureIF figure, Pen pen)
+			{
+				pen.Color = figure.getColor();//перекрываю метод фабричный
+				paint_box.CreateGraphics().DrawEllipse(pen, figure.getX() - getRad(),
+						figure.getY() - getRad(), getRad() * 2, getRad() * 2);
+			}
+			public override void changeSize(int k)
+			{
+				СircleRad += k;
+			}
+			public override void moveX(int k)
+			{
+				if (getX() - СircleRad > 0)
+				{
+					this.setX(k);
+				}
+				else
+					this.setX(Math.Abs(k));
+			}
+			public override void moveY(int k)
+			{
+				if (getY() - СircleRad > 0)
+				{
+					this.setY(k);
+				}
+				else
+					this.setY(Math.Abs(k));
+			}
+		}
+
+		class mystorage
+		{
+			private FigureIF head;//головной узел
+			private FigureIF next;//следующий узел
+			private FigureIF end;//последний узел
+			private int N;//кол-во
+			public mystorage()
+			{//конструктор
+				head = null;// список пустой
+				N = 0;//кол-во
+			}
+
+			public int getN()
+			{
+				return N;
+			}
+			public void add(FigureIF obj)
+			{//добавляем новый объект
+				next = null;
+				if (head == null)//если первый раз добавляем
+					head = obj;
+				else
+					end.setnext(obj);
+				end = obj;
+				N++;//добавляем число объектов
+			}
+
+			public FigureIF First()
+			{
+				next = head;//присваиваю next головному узлу
+				return next;
+			}
+
+			public FigureIF Next()
+			{
+				next = next.getnext();//присваиваю next след.
+				return next;
+			}
+
+			public bool EOL()
+			{//проверка на пустоту
+				if (next == null)
+					return false;
+				return true;
+			}
+			public FigureIF getobj()
+			{//возвращаю next
+				return next;
+			}
+
+			public FigureIF getobj(FigureIF obj)
+			{//возвращаю next, если 
+				for (First(); EOL(); Next())//нашли next = obj
+					if (next == obj)
+						return next;
+				return null;//иначе выводим NULL
+			}
+
+			public void delobj(FigureIF selobj)
+			{//удаление объекта
+				First();
+				FigureIF back = head;
+				while (EOL())//пока не конец
+				{
+					if (next == selobj)//если находим selobj
+						break;
+					back = next;//
+					Next();
+				}
+				if (selobj == head || selobj == end)//передаем все свойства объекта перед удалением 
+				{
+					if (selobj == head)//если это головной 
+					{
+						head = head.getnext();
+						back = head;
+					}
+					if (selobj == end)//если это последний 
+					{
+						end = back;
+						if (end != null)
+							end.setnext(null);
+					}
+				}
+				else
+					back.setnext(selobj.getnext());
+
+				selobj = null;
+				N--;
+			}
+		}
+
+		public bool clickCheck(MouseEventArgs e)
+		{
+			for (Storage.First(); Storage.EOL(); Storage.Next())//нашли next = obj 
+			{
+				if (!ModifierKeys.HasFlag(Keys.Control) && Storage.getobj().getColor() == Color.Orange)
+				{//если не нажат ctrl и цвет объекта оранжевый
+					printFigure(Storage.getobj(), Color.Red);
+				}
+			}
+
+			bool check = false;
+			for (Storage.First(); Storage.EOL(); Storage.Next())//нашли next = obj 
+			{
+				if(Storage.getobj() is Line //если это линия и условие нажатие на линию
+					&& e.X >= Storage.getobj().getX() - ((Line)Storage.getobj()).getLineLength()
+					&& e.X <= Storage.getobj().getX() + ((Line)Storage.getobj()).getLineLength()
+					&& e.Y >= Storage.getobj().getY() - 2
+					&& e.Y <= Storage.getobj().getY() + 2)
+				{
+					printFigure(Storage.getobj(), Color.Orange);
+					check = true;
+				}
+				if (Storage.getobj() is Сircle && //если это круг и условие нажатие на круг
+					(Storage.getobj().getX() - e.X )* (Storage.getobj().getX() - e.X )+
+					(Storage.getobj().getY() - e.Y) * (Storage.getobj().getY() - e.Y) 
+					<= ((Сircle)Storage.getobj()).getRad() * ((Сircle)Storage.getobj()).getRad())
+				{
+					printFigure(Storage.getobj(), Color.Orange);
+					check = true;
+				}
+			}
+			return check;
+		}
+
+		private void paint_box_MouseClick(object sender, MouseEventArgs e)
+		{
+			
+			if (clickCheck(e)) //проверка 
+			{
+				return;
+			}
+			FigureIF krug; // создание объекта
+			if (radioButton1.Checked == true)
+			{
+				krug = new Сircle(e.X, e.Y);//если нажимаю на пустое место - создаю круг
+			}
+			else
+			{
+				krug = new Line(e.X, e.Y);//если нажимаю на пустое место - создаю линию
+			}
+
+			printFigure(krug, Color.Orange); //метод printFigure
+			label_paintbox.Visible = false;
+
+			Storage.add(krug);//добавляю в хранилище
+		}
+
+		private void Main_KeyDown(object sender, KeyEventArgs e)
+		{
+			int k = 5;
+			if (e.KeyCode.Equals(Keys.A) || e.KeyCode.Equals(Keys.D))
+			{ //если нажал влево или вправо 
+				for (Storage.First(); Storage.EOL(); Storage.Next())//нашли next = obj 
+				{
+					if (Storage.getobj().getColor() == Color.Orange && e.KeyCode.Equals(Keys.A))
+						Storage.getobj().moveX(-k); //если влево
+					else if(Storage.getobj().getColor() == Color.Orange)
+						Storage.getobj().moveX(k); //если вправо
+				}
+				paint_box.Refresh();
+				for (Storage.First(); Storage.EOL(); Storage.Next())//нашли next = obj 
+					Storage.getobj().FactoryPaint(paint_box, Storage.getobj(), pen);
+			}
+			else if (e.KeyCode.Equals(Keys.W) || e.KeyCode.Equals(Keys.S))
+			{ //если нажал вверх или вниз
+				for (Storage.First(); Storage.EOL(); Storage.Next())//нашли next = obj 
+				{
+					if (Storage.getobj().getColor() == Color.Orange && e.KeyCode.Equals(Keys.W))
+						Storage.getobj().moveY(-k); //если вверх
+					else if(Storage.getobj().getColor() == Color.Orange)
+						Storage.getobj().moveY(k); //если вниз
+				}
+				paint_box.Refresh();
+				for (Storage.First(); Storage.EOL(); Storage.Next())//нашли next = obj 
+					Storage.getobj().FactoryPaint(paint_box, Storage.getobj(), pen);
+			}
+			else if(e.KeyCode.Equals(Keys.Add) || e.KeyCode.Equals(Keys.Subtract))
+			{ //если нажал + или -
+				for (Storage.First(); Storage.EOL(); Storage.Next())//нашли next = obj 
+				{
+					if (Storage.getobj().getColor() == Color.Orange && e.KeyCode.Equals(Keys.Add))
+						Storage.getobj().changeSize(k);
+					else if (Storage.getobj().getColor() == Color.Orange)
+						Storage.getobj().changeSize(-k);
+				}
+				paint_box.Refresh();
+				for (Storage.First(); Storage.EOL(); Storage.Next())//нашли next = obj 
+					Storage.getobj().FactoryPaint(paint_box, Storage.getobj(), pen);
+			}
+			else if (e.KeyCode.Equals(Keys.Delete))
+			{ //если нажал delete
+				for (Storage.First(); Storage.EOL(); Storage.Next())//нашли next = obj 
+				{
+					if (Storage.getobj().getColor() == Color.Orange)
+					{
+						Storage.delobj(Storage.getobj());//удаляю объект выделенный
+						continue;
+					}
+				}
+				paint_box.Refresh();
+				for (Storage.First(); Storage.EOL(); Storage.Next())//нашли next = obj 
+				{
+					Storage.getobj().FactoryPaint(paint_box, Storage.getobj(), pen);
+				}
+			}
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			if (colorDialog1.ShowDialog() != System.Windows.Forms.DialogResult.Cancel)
+				textBox4.BackColor = colorDialog1.Color; //изменение цвета
+		}
+
+		private void textBox4_Click(object sender, EventArgs e)
+		{
+			for (Storage.First(); Storage.EOL(); Storage.Next())//нашли next = obj 
+			{
+				if (Storage.getobj().getColor() == Color.Orange)
+				{
+					Storage.getobj().setColor(((TextBox)sender).BackColor);
+				} //изменение цвета
+			}
+			paint_box.Refresh();
+			for (Storage.First(); Storage.EOL(); Storage.Next())//нашли next = obj 
+			{
+				Storage.getobj().FactoryPaint(paint_box, Storage.getobj(), pen);
+			}
+		}
+
+	}
+}
