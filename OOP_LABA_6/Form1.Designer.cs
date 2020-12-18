da1e657c5ccb8be1bbcac63c3f16322178e6e8ab
ildar1rap <61325972+ildar1rap@users.noreply.github.com>
2020-12-18 17:54:01 +0500

    1
    1

--- OOP_LABA_6/Form1.Designer.cs
+++ OOP_LABA_6/Form1.Designer.cs
@@ -40,6 +40,10 @@
             this.button1 = new System.Windows.Forms.Button();
             this.colorDialog1 = new System.Windows.Forms.ColorDialog();
             this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.input = new System.Windows.Forms.Button();
+            this.output = new System.Windows.Forms.Button();
+            this.textBox5 = new System.Windows.Forms.TextBox();
             this.paint_box.SuspendLayout();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
@@ -167,11 +171,55 @@
             this.textBox4.TabIndex = 15;
             this.textBox4.Click += new System.EventHandler(this.textBox4_Click);
             // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(642, 342);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(161, 46);
+            this.button2.TabIndex = 16;
+            this.button2.Text = "Группировка ";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // input
+            // 
+            this.input.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.input.Location = new System.Drawing.Point(642, 394);
+            this.input.Name = "input";
+            this.input.Size = new System.Drawing.Size(161, 37);
+            this.input.TabIndex = 17;
+            this.input.Text = "input";
+            this.input.UseVisualStyleBackColor = true;
+            this.input.Click += new System.EventHandler(this.input_Click);
+            // 
+            // output
+            // 
+            this.output.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.output.Location = new System.Drawing.Point(642, 437);
+            this.output.Name = "output";
+            this.output.Size = new System.Drawing.Size(161, 37);
+            this.output.TabIndex = 18;
+            this.output.Text = "output";
+            this.output.UseVisualStyleBackColor = true;
+            this.output.Click += new System.EventHandler(this.output_Click);
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(642, 492);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 20);
+            this.textBox5.TabIndex = 19;
+            // 
             // Main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(844, 552);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.output);
+            this.Controls.Add(this.input);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.textBox4);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox3);
@@ -206,6 +254,10 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.ColorDialog colorDialog1;
         private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button input;
+        private System.Windows.Forms.Button output;
+        private System.Windows.Forms.TextBox textBox5;
     }
 }
 
--- OOP_LABA_6/Form1.cs
+++ OOP_LABA_6/Form1.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace OOP_LABA_6
 {
@@ -19,77 +20,234 @@ namespace OOP_LABA_6
         {
 			InitializeComponent();
         }
-		public class FigureIF //класс фигур
+		/// <summary>
+		/// Группа может : Двигаться
+		///				   Менять цвет
+		///				   Менять размер
+		///				   Отрисовываться 
+		/// </summary>
+		public abstract class FigureAbstract 
+		{
+			public virtual string save()
+			{
+				return "";
+			}
+			public  FigureAbstract _next;//указатель на некст объект
+			public Color FigureIFColor = Color.Red;//цвет;
+			public Color getColor()
+			{
+				return FigureIFColor;
+			}
+			public virtual void setnext(FigureAbstract obj)//метод set next
+			{//задаем _next
+				_next = obj;
+			}
+			public virtual FigureAbstract getnext() //метод возвращающий указатель не след.
+			{//возвращает _next
+				return _next;
+			}
+			public virtual void FactoryPaint(Panel paint_box, FigureAbstract figure, Pen pen) { }
+			
+			public virtual void moveX(int k) { }
+			public virtual void moveY(int k) { }
+			public virtual void changeSize(int k) { }
+			public virtual int getX()
+			{
+				return 0;
+			}
+			public virtual int getY()
+			{
+				return 0;
+			}
+
+			public virtual void setColor(Color changeColor) { }
+			public virtual void setX(int k) { }
+			public virtual void setY(int k) { }
+			public virtual bool checkFigure(FigureAbstract figure, MouseEventArgs e, Panel paint_box, Pen pen) 
+			{
+				return false;
+			}
+			public virtual int getsize() { return 0; }
+		}
+		public class factory : FigureAbstract
+		{
+			public virtual void FactoryPaintNEW(Panel paint_box, Pen pen, string code, string x, string y, string dlina)
+			{
+				int x1=Convert.ToInt32(x), y1=Convert.ToInt32(y), dlina1= Convert.ToInt32(dlina);
+				if (code != null)
+				{
+					switch (code)
+					{
+						case "Line":
+							FigureAbstract figure = new Line(x1, y1,dlina1);
+							figure.FactoryPaint(paint_box, figure, pen);
+							break;
+						case "Сircle":
+							FigureAbstract figure1 = new Сircle(x1, y1, dlina1);
+							figure1.FactoryPaint(paint_box, figure1, pen);
+							break;
+						default:
+							break;
+					}
+				}
+			}
+		}
+		public class GroupFigures : FigureAbstract
+		{
+			public override string save()
+			{
+				string save1= "<> ";
+				for (int i = 0; i < count; i++)
+				{
+					save1 += group[i].save();
+				}
+				save1 += " </> ";
+				return save1;
+			}
+			public int maxcount = 10;
+			int count;
+			public FigureAbstract[] group; //public
+			public GroupFigures(Color color)//конструктор с параметрами
+			{
+				FigureIFColor = color;
+				count = 0;
+				group = new FigureAbstract[maxcount];
+			}
+			public void GroupAddFigure(FigureAbstract newOBJECT)
+			{
+				if (count >= maxcount)
+					return;
+
+				count++;
+				group[count - 1] = newOBJECT;
+			}
+			public override void setColor(Color changeColor) 
+			{
+				FigureIFColor = changeColor;
+				for (int i = 0; i < count; i++)
+				{
+					group[i].setColor(changeColor);
+				}
+			}
+			public override void setX(int k)
+			{
+				for (int i = 0; i < count; i++)
+				{
+					group[i].setX(k);
+				}
+			}
+			public override void setY(int k)
+			{
+				for (int i = 0; i < count; i++)
+				{
+					group[i].setY(k);
+				}
+			}
+			public override void moveX(int k) 
+			{
+				for (int i = 0; i < count; i++)
+				{
+					group[i].setX(k);
+				}
+			}
+			public override void moveY(int k)
+			{
+				for (int i = 0; i < count; i++)
+				{
+					group[i].setY(k);
+				}
+			}
+			public override bool checkFigure(FigureAbstract figure, MouseEventArgs e, Panel paint_box, Pen pen)
+			{
+				for (int i = 0; i < count; i++)
+				{
+					if (group[i].checkFigure(group[i], e, paint_box, pen))
+					{
+						this.FigureIFColor = Color.Orange;
+						figure.FactoryPaint(paint_box, figure, pen);
+						return true;
+					}
+				}
+				return false;
+			}
+			public override void changeSize(int k) 
+			{
+				for (int i = 0; i < count; i++)
+				{
+					group[i].changeSize(k);
+				}
+			}
+			public override void FactoryPaint(Panel paint_box, FigureAbstract figure, Pen pen) 
+			{
+				for (int i = 0; i < count; i++)
+				{
+					group[i].FigureIFColor = this.FigureIFColor;
+					group[i].FactoryPaint(paint_box, group[i], pen);
+				}
+			}
+		}
+
+
+		public class FigureIF : FigureAbstract //класс фигур
 		{
 			private int x, y; // точки
-			private Color FigureIFColor = Color.Red;//цвет;
-			private FigureIF _next;//указатель на некст объект
+
 			public FigureIF(int x, int y)//конструктор с параметрами
 			{
 				this.x = x;
 				this.y = y;
 			}
-			public int getX()
+			public override int getX()
 			{
 				return x;
 			}
-			public void setX(int k)
+			public override void setX(int k)
 			{
 				x+=k;
 			}
-			public int getY()
+			public override int getY()
 			{
 				return y;
 			}
-			public void setY(int k)
+			public override void setY(int k)
 			{
 				y += k;
 			}
-			public Color getColor()
-			{
-				return FigureIFColor;
-			}
-			public void setColor(Color changeColor)
+			public override void setColor(Color changeColor)
 			{
 				FigureIFColor = changeColor;
 			}
-			public void setnext(FigureIF obj)//метод set next
-			{//задаем _next
-				_next = obj;
-			}
-			public FigureIF getnext() //метод возвращающий указатель не след.
-			{//возвращает _next
-				return _next;
-			}
-			public virtual void FactoryPaint(Panel paint_box, FigureIF figure, Pen pen) 
-			{ } //фабричный метож
-			public virtual void moveX(int k)
-			{
-				x += k;
-			}
-			public virtual void moveY(int k)
-			{
-				y += k;
-			}
-			public virtual void changeSize(int k) { }
 		}
-		public void printFigure(FigureIF obj, Color color)//метод для отрисовки 
+		public void printFigure(FigureAbstract obj, Color color)//метод для отрисовки 
 		{
 			obj.setColor(color);
 			obj.FactoryPaint(paint_box, obj, pen);
 		}
 		class Line : FigureIF
 		{
+			public override string save()
+			{
+				return "Line";
+			}
 			private int LineLength = 50; // длина линии
+			public override int getsize() { return LineLength; }
 			public Line(int x, int y) : base(x, y)//конструктор с параметрами
 			{
 
+			}
+			public Line(int x, int y, int dlina) : base(x, y)//конструктор с параметрами
+			{
+				LineLength = dlina;
 			}
 			public int getLineLength()
 			{
 				return LineLength;
 			}
-			public override void FactoryPaint(Panel paint_box, FigureIF figure, Pen pen)
+			public void setLineLength(int LineLength)
+			{
+				this.LineLength=LineLength;
+			}
+			public override void FactoryPaint(Panel paint_box, FigureAbstract figure, Pen pen)
 			{
 				pen.Color = figure.getColor();//перекрываю метод, фабричного метода
 				paint_box.CreateGraphics().DrawLine(pen, figure.getX() - getLineLength(),
@@ -117,20 +275,44 @@ namespace OOP_LABA_6
 				else
 					this.setY(Math.Abs(k));
 			}
+			public override bool checkFigure(FigureAbstract figure, MouseEventArgs e, Panel paint_box, Pen pen)
+			{
+				if (e.X >= figure.getX() - getLineLength()
+					&& e.X <= figure.getX() + getLineLength()
+					&& e.Y >= figure.getY() - 2
+					&& e.Y <= figure.getY() + 2)
+				{
+					return true;
+				}
+				else return false;
+			}
 		}
 
 		class Сircle : FigureIF
         {
+			public override string save()
+			{
+				return "Сircle";
+			}
 			private int СircleRad = 30; //радиус круга
-            public Сircle(int x, int y) : base(x,y)//конструктор с параметрами
+			public override int getsize() { return СircleRad; }
+			public Сircle(int x, int y) : base(x,y)//конструктор с параметрами
             {
 
-            }
+			}
+			public Сircle(int x, int y, int dlina) : base(x, y)//конструктор с параметрами
+			{
+				СircleRad = dlina;
+			}
 			public int getRad()
 			{
 				return СircleRad;
 			}
-			public override void FactoryPaint(Panel paint_box, FigureIF figure, Pen pen)
+			public void setRad(int СircleRad)
+			{
+				this.СircleRad = СircleRad;
+			}
+			public override void FactoryPaint(Panel paint_box, FigureAbstract figure, Pen pen)
 			{
 				pen.Color = figure.getColor();//перекрываю метод фабричный
 				paint_box.CreateGraphics().DrawEllipse(pen, figure.getX() - getRad(),
@@ -158,25 +340,34 @@ namespace OOP_LABA_6
 				else
 					this.setY(Math.Abs(k));
 			}
+			public override bool checkFigure(FigureAbstract figure, MouseEventArgs e, Panel paint_box, Pen pen)
+			{
+				if ((figure.getX() - e.X) * (figure.getX() - e.X) +
+					(figure.getY() - e.Y) * (figure.getY() - e.Y)
+					<= getRad() * getRad())
+				{
+					return true;
+				}
+				else return false;
+			}
 		}
 
-		class mystorage
+		public class mystorage
 		{
-			private FigureIF head;//головной узел
-			private FigureIF next;//следующий узел
-			private FigureIF end;//последний узел
+			private FigureAbstract head;//головной узел
+			private FigureAbstract next;//следующий узел
+			private FigureAbstract end;//последний узел
 			private int N;//кол-во
 			public mystorage()
 			{//конструктор
 				head = null;// список пустой
 				N = 0;//кол-во
 			}
-
 			public int getN()
 			{
 				return N;
 			}
-			public void add(FigureIF obj)
+			public void add(FigureAbstract obj)
 			{//добавляем новый объект
 				next = null;
 				if (head == null)//если первый раз добавляем
@@ -187,13 +378,13 @@ namespace OOP_LABA_6
 				N++;//добавляем число объектов
 			}
 
-			public FigureIF First()
+			public FigureAbstract First()
 			{
 				next = head;//присваиваю next головному узлу
 				return next;
 			}
 
-			public FigureIF Next()
+			public FigureAbstract Next()
 			{
 				next = next.getnext();//присваиваю next след.
 				return next;
@@ -205,23 +396,15 @@ namespace OOP_LABA_6
 					return false;
 				return true;
 			}
-			public FigureIF getobj()
+			public FigureAbstract getobj()
 			{//возвращаю next
 				return next;
 			}
 
-			public FigureIF getobj(FigureIF obj)
-			{//возвращаю next, если 
-				for (First(); EOL(); Next())//нашли next = obj
-					if (next == obj)
-						return next;
-				return null;//иначе выводим NULL
-			}
-
-			public void delobj(FigureIF selobj)
+			public void delobj(FigureAbstract selobj)
 			{//удаление объекта
 				First();
-				FigureIF back = head;
+				FigureAbstract back = head;
 				while (EOL())//пока не конец
 				{
 					if (next == selobj)//если находим selobj
@@ -249,10 +432,22 @@ namespace OOP_LABA_6
 				selobj = null;
 				N--;
 			}
+
+			public void adddown(FigureAbstract obj, FigureAbstract select)
+			{
+				FigureAbstract nex = select.getnext();
+				if (nex != null)
+					obj.setnext(nex);
+				select.setnext(obj);
+				if (end == select)
+					end = obj;
+				this.N++;
+			}
 		}
 
 		public bool clickCheck(MouseEventArgs e)
 		{
+			bool check = false;
 			for (Storage.First(); Storage.EOL(); Storage.Next())//нашли next = obj 
 			{
 				if (!ModifierKeys.HasFlag(Keys.Control) && Storage.getobj().getColor() == Color.Orange)
@@ -261,22 +456,9 @@ namespace OOP_LABA_6
 				}
 			}
 
-			bool check = false;
 			for (Storage.First(); Storage.EOL(); Storage.Next())//нашли next = obj 
 			{
-				if(Storage.getobj() is Line //если это линия и условие нажатие на линию
-					&& e.X >= Storage.getobj().getX() - ((Line)Storage.getobj()).getLineLength()
-					&& e.X <= Storage.getobj().getX() + ((Line)Storage.getobj()).getLineLength()
-					&& e.Y >= Storage.getobj().getY() - 2
-					&& e.Y <= Storage.getobj().getY() + 2)
-				{
-					printFigure(Storage.getobj(), Color.Orange);
-					check = true;
-				}
-				if (Storage.getobj() is Сircle && //если это круг и условие нажатие на круг
-					(Storage.getobj().getX() - e.X )* (Storage.getobj().getX() - e.X )+
-					(Storage.getobj().getY() - e.Y) * (Storage.getobj().getY() - e.Y) 
-					<= ((Сircle)Storage.getobj()).getRad() * ((Сircle)Storage.getobj()).getRad())
+				if (Storage.getobj().checkFigure(Storage.getobj(), e, paint_box, pen))
 				{
 					printFigure(Storage.getobj(), Color.Orange);
 					check = true;
@@ -292,20 +474,20 @@ namespace OOP_LABA_6
 			{
 				return;
 			}
-			FigureIF krug; // создание объекта
+			FigureIF obj; // создание объекта
 			if (radioButton1.Checked == true)
 			{
-				krug = new Сircle(e.X, e.Y);//если нажимаю на пустое место - создаю круг
+				obj = new Сircle(e.X, e.Y);//если нажимаю на пустое место - создаю круг
 			}
 			else
 			{
-				krug = new Line(e.X, e.Y);//если нажимаю на пустое место - создаю линию
+				obj = new Line(e.X, e.Y);//если нажимаю на пустое место - создаю линию
 			}
 
-			printFigure(krug, Color.Orange); //метод printFigure
+			printFigure(obj, Color.Orange); //метод printFigure
 			label_paintbox.Visible = false;
 
-			Storage.add(krug);//добавляю в хранилище
+			Storage.add(obj);//добавляю в хранилище
 		}
 
 		private void Main_KeyDown(object sender, KeyEventArgs e)
@@ -390,5 +572,81 @@ namespace OOP_LABA_6
 			}
 		}
 
+		private void button2_Click(object sender, EventArgs e)
+		{
+			GroupFigures newOBJECT = new GroupFigures(Color.Orange);
+			bool check = true;
+			for (Storage.First(); Storage.EOL(); Storage.Next())//нашли next = obj 
+			{
+				if(Storage.getobj().getColor() == Color.Orange)
+				{
+					newOBJECT.GroupAddFigure(Storage.getobj());
+					if (check)
+					{
+						Storage.adddown(newOBJECT, Storage.getobj());
+						Storage.delobj(Storage.getobj());
+						check = false;
+						Storage.Next();
+						continue;
+					}
+					Storage.delobj(Storage.getobj()); 
+					
+				}
+			}
+		}
+
+		private void output_Click(object sender, EventArgs e)
+		{
+			paint_box.Refresh();
+			const Int32 BufferSize = 128;
+			using (StreamWriter streamWriter = new StreamWriter(path))
+			{
+				streamWriter.WriteLine(Storage.getN());
+				for (Storage.First(); Storage.EOL(); Storage.Next())//нашли next = obj 
+				{
+					streamWriter.WriteLine(Storage.getobj().save());
+					streamWriter.WriteLine(Storage.getobj().getX());
+					streamWriter.WriteLine(Storage.getobj().getY());
+					streamWriter.WriteLine(Storage.getobj().getsize());
+				}
+			}
+
+		}
+
+		string path = @"C:\OOP_LABA7\filename.txt";
+
+		private void input_Click(object sender, EventArgs e)
+		{
+			String smth,code="", x="", y="", dlina=""; int count = 0, count1 =0;
+			const Int32 BufferSize = 128;
+			using (var fileStream = File.OpenRead(path))
+			using (var streamReader = new StreamReader(fileStream, Encoding.UTF8, true, BufferSize))
+			{
+				count1 = Convert.ToInt32(streamReader.ReadLine());
+				count1 = count1*4+1;
+				while (count1 != 0)
+				{
+					if(count >3)
+   					{
+						count = 0;
+						factory fact = new factory();
+						fact.FactoryPaintNEW(paint_box, pen, code, x, y, dlina);
+						continue;
+					}
+					smth = streamReader.ReadLine();
+					if (count == 0)
+						code = smth;
+					else if(count == 1)
+						x = smth;
+					else if(count == 2)
+						y = smth;
+					else if(count == 3)
+						dlina = smth;
+					
+					count++;
+					count1--;
+				}
+			}
+		}
 	}
 }
